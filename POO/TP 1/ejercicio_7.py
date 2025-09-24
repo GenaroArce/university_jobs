9011c182bd675c91357219561f9661165934e170
Genaro <genaroarcee@gmail.com>
2025-09-24 15:13:43 -0300

    Ejercicio 7 TP_1 - POO Resuelto

--- POO/TP 1/ejercicio_7.py
+++ POO/TP 1/ejercicio_7.py
@@ -1,19 +1,26 @@
 class App:
     def __init__(self, usuario):
         self.__usuario = usuario
+        self.__blacklist = ['admin', 'root', 'user', 'administrador']
         self.check()
     
     def verificacion_caracteres(self):
         return len(self.__usuario) >= 6
     
     def verificacion_letras(self):
-        return self.__usuario == self.__usuario.capitalize()
+        return self.__usuario[0].isupper() and self.__usuario[1:].islower()
         
     def verificacion_espacios(self):
-        return self.__usuario == self.__usuario.strip()
+        return self.__usuario == self.__usuario.strip() and " " not in self.__usuario
         
     def verificacion_numeros(self):
         return not any(char.isdigit() for char in self.__usuario)
+    
+    def verificacion_especial(self):
+        return self.__usuario.isalpha()
+    
+    def verificacion_prohibido(self):
+        return self.__usuario.lower() in self.__blacklist
 
     def check(self):
         lista_errores = []
@@ -26,6 +33,10 @@ class App:
             lista_errores.append("El usuario no debe de contener espacios.")
         elif not self.verificacion_numeros():
             lista_errores.append("El usuario no debe de contener numeros.")
+        elif not self.verificacion_especial():
+            lista_errores.append("El usuario no debe de contener caracteres especiales.")
+        elif self.verificacion_prohibido():
+            lista_errores.append("El usuario esta en la lista negra.")
         
         if lista_errores:
             print(f"El usuario {self.__usuario} no cumple con las condiciones: ")
